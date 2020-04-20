e4ef5fdcc498a2ab736acf151f15acf46f31816b
SchneeHertz <snowhzhang@gmail.com>
2020-04-20 16:49:46 +0800

    add konva libaray

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import './style/theme/index.css'
 import './assets/icon/iconfont.css'
 import _ from 'lodash'
 import axios from 'axios'
+import VueKonva from 'vue-konva'
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(VueKonva)
 window._ = _
 
 Vue.config.productionTip = false

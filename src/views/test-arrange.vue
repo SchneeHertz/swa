b6da1feb5ec88b71296f0c5e9d51e9b75b0a4ed8
SchneeHertz <snowhzhang@gmail.com>
2020-07-21 08:58:22 +0800

    add point and group list for group page

--- src/views/test-arrange.vue
+++ src/views/test-arrange.vue
@@ -1,41 +1,75 @@
 <template>
   <el-container class="test-arrange-page">
     <BaseHeader activeIndex="test-arrange"/>
-    <div style="height:100vh;width:100vw;">
+    <el-main>
       <el-row>
-        <el-col :span="4" style="height:100vh; border-right: solid 1px lightgrey;">
-          <draggable
-            class="point-list-group"
-            :group="{ name: 'point', pull: 'clone', put: false }"
-            :clone="clonePoint"
-            v-model="pointList"
-            v-bind="dragOptions"
-            @start="drag = true"
-            @end="drag = false"
-          >
-            <transition-group type="transition" :name="!drag ? 'flip-list' : null">
-              <div 
+        <el-col :span="4" class="point-list">
+          <overlay-scrollbars
+            :options="{scrollbars: {autoHide: 'scroll'}}"
+            class="ol-point-list"
+            >
+            <draggable
+              class="point-list-group"
+              :group="{ name: 'point', pull: 'clone', put: false }"
+              :clone="clonePoint"
+              v-model="pointList"
+              v-bind="dragOptions"
+            >
+              <div
                 v-for="point in pointList"
                 :key="point.id"
                 class="point-list-item"
               >
-                {{point.englishDescription}}
+                {{point.index}}. {{point.englishDescription}}
               </div>
-            </transition-group>
-          </draggable>
+            </draggable>
+          </overlay-scrollbars>
         </el-col>
-        <el-col :span="14" style="height:100vh; border-right: solid 1px lightgrey;">
-          <el-card
-            class="group-card"
-            v-for="group in pointGroupList"
-            :key="group.id"
+        <el-col :span="14" class="group-list">
+          <overlay-scrollbars
+            :options="{scrollbars: {autoHide: 'scroll'}}"
+            class="ol-group-list"
           >
-            <group-nest v-model="group.list" />
-          </el-card>
+            <el-card
+              class="group-card"
+              v-for="group in pointGroupList"
+              :key="group.id"
+            >
+              <template #header>
+                <el-input
+                  class="group-index"
+                  v-model="group.index"
+                  size="mini"
+                ></el-input>
+                <el-select
+                  size="mini"
+                  v-model="test"
+                  placeholder="Sub Clause"
+                >
+                </el-select>
+                <el-button type="text" class="close-circle-button" icon="el-third-icon-close" @click="removeGroup(group.id)" plain/>
+              </template>
+              <group-nest v-model="group.list" />
+            </el-card>
+            <el-card class="add-group-card">
+              <el-button type="primary" class="bigicon add-button" icon="el-third-icon-reload" circle title="排序" @click="reSortGroupList"></el-button>
+              <el-button type="success" class="bigicon add-button" icon="el-third-icon-plus" circle title="新增" @click="addGroup(1)"></el-button>
+              <el-button type="success" class="bigicon add-button" icon="el-third-icon-rocket" circle title="新增10个" @click="addGroup(10)"></el-button>
+              <el-button type="success" class="bigicon add-button" icon="el-third-icon-infomation" circle title="新增文本组" @click="addTextGroup"></el-button>
+            </el-card>
+          </overlay-scrollbars>
         </el-col>
-        <el-col :span="6"><div style="height:100vh;"></div></el-col>
+        <el-col :span="6">
+          <div class="method-pane"></div>
+          <div class="regulation-pane"></div>
+          <div class="bottom-function-btn">
+            <el-button type="primary" class="bigicon" icon="el-third-icon-cloud-download" circle @click="loadTasklist" title="载入"></el-button>
+            <el-button type="success" class="bigicon" icon="el-third-icon-save" circle @click="saveTasklist" title="保存"></el-button>
+            <el-button type="primary" class="bigicon" icon="el-third-icon-right" circle title="下一步" @click="toNextPage"></el-button>
+          </div>
+        </el-col>
       </el-row>
-    </div>
+    </el-main>
   </el-container>
 </template>
 
@@ -44,6 +78,8 @@
 import BaseHeader from '@/components/BaseHeader.vue'
 import GroupNest from '@/components/GroupNest.vue'
 import draggable from 'vuedraggable'
+
+import {generate as _id } from 'shortid'
 
 function geneVuexValue (property) {
   return {
@@ -62,16 +98,13 @@ export default {
   },
   data () {
     return {
-      pointGroupList: [{
-        "id": 1,
-        "list": []
-      }],
+      pointGroupList: [],
       dragOptions: {
         animation: 200,
         disabled: false,
         ghostClass: "ghost"
       },
-      drag: false
+      test: ''
     }
   },
   computed: {
@@ -84,33 +117,120 @@ export default {
 
   },
   methods: {
+    geneMethodList () {
+      
+    },
     clonePoint (point) {
       return _.merge(_.cloneDeep(point), {elements: []})
-    }
+    },
+    findMinIndex (array) {
+      let result
+      _.forOwn(_.sortBy(array, e=>+e), (v,i)=>{
+        if (+v != +i+1) {
+          result = +i+1
+          return false
+        }
+      })
+      return result ? result : array.length+1
+    },
+    addGroup (count = 1) {
+      for (let i = 0; i < count; i++) {
+        let index = this.findMinIndex(this.pointGroupList.map(e=>e.index))
+        this.pointGroupList.push({
+          id: _id(),
+          index: index,
+          list: []
+        })
+      }
+    },
+    addTextGroup () {
+
+    },
+    reSortGroupList () {
+      this.pointGroupList = _.sortBy(this.pointGroupList, 'index')
+    },
+    removeGroup (id) {
+      let foundGroup = _.findIndex(this.pointGroupList, {id: id})
+      if (foundGroup != -1) {
+        this.pointGroupList.splice(foundGroup, 1)
+      }
+    },
+    loadTasklist () {
+
+    },
+    saveTasklist () {
+
+    },
+    toNextPage () {
+
+    },
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.flip-list-move
-  transition: transform 0.5s
-.no-move
-  transition: transform 0s
+.el-main
+  padding: 0
 .ghost
   opacity: 0.5
-  background: #ffffff
+.add-group-card
+  width: 21em
+  height: 6em
+  display: inline-block
+  margin: 4px
+  text-align: center
+.add-group-card .add-button
+  position: relative
+  top: 1em
+.method-pane
+  height: 40vh
+  border-bottom: solid 1px rgba(0,0,0,0.125)
+.regulation-pane
+  height: 52vh
+.bottom-function-btn
+  position: absolute
+  bottom: 1em
+  right: 1.5em
 </style>
 
 <style lang="stylus">
 .test-arrange-page
+  .point-list
+    border-right: solid 1px rgba(0,0,0,0.125)
+  .ol-point-list
+    height: 100vh
   .point-list-item
-    margin: 0
-    padding: 10px 8px
+    margin: 1px
+    padding: 6px
     border: solid 1px rgba(0,0,0,0.125)
-    border-right-width: 0
-  .point-list-item+.point-list-item
-    border-top-width: 0
-  .group-card  
-    width: 48%
-
+    border-radius: 4px
+    background-color: #f5f5f5
+    font-size: 14px
+    cursor: move
+  
+  .group-list
+    border-right: solid 1px rgba(0,0,0,0.125)
+    .ol-group-list
+      height: 100vh
+    .el-card.group-card
+      margin: 4px 6px
+      width: 21em
+      display: inline-block
+      vertical-align: top
+    .el-card__header
+      padding: 6px 10px
+      .group-index
+        display: inline-block
+        width: 3.2em
+        .el-input__inner
+          padding: 0 5px
+          text-align: center
+      .el-select
+        width: 15.5em
+      .close-circle-button
+        padding: 2px
+        margin: 3px 0
+        float: right
+    .el-card__body
+      padding: 6px
 </style>
